5b61d4ebcbd8d23ac4c614670253d86f04aecaf5
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-25 01:04:51 -0500

    create home page component

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = function() {
+  return (
+    <div>
+      <a href='./'>Chattaway</a>
+      <input type='text' placeholder='Search...' />
+      <a href='./signin'><span>Sign in</span></a>
+    </div>
+  )
+}
