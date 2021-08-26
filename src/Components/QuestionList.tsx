3477ed209efe85fe7c96a3bfa02ff059a69d3964
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-25 23:38:09 -0500

    Created questionlist component

---
+++ src/Components/QuestionList.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { QuestionData } from '../MockData/QuestionsData'
+
+interface Props {
+  data: QuestionData[]
+}
+
+export const QuestionList = function({data}: Props) {
+  return (
+    <ul>
+      {data.map(function(question) {
+        return (
+          <li>key={question.questionId}</li>
+        )
+      })}
+    </ul>
+  )
+}
