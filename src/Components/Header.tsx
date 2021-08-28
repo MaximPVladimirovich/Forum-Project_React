3ccd40fc7efef8ac6145a201312b46fd3c1e8d1b
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-28 11:32:58 -0500

    Added header searchHandleChange

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export const Header = function() {
+
+  const handleSearchInputChange = function(e: React.ChangeEvent<HTMLInputElement>) {
+    return console.log(e.currentTarget.value)
+  }
+
   return (
     <div>
       <a href='./'>Chattaway</a>
-      <input type='text' placeholder='Search...' />
+      <input type='text' placeholder='Search...' onChange={handleSearchInputChange}/>
       <a href='./signin'><span>Sign in</span></a>
     </div>
   )
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -7,21 +7,24 @@ export const HomePage = function() {
   const [questions, setQuestions] = React.useState<QuestionData[]>([])
   const [questionsLoading, setQuestionsLoading] = React.useState(true)
 
-  React.useEffect( () => {
+  React.useEffect(() => {
     const doGetUnansweredQuestions = async () => {
       const unansweredQuestions = await getUnansweredQuestions()
       setQuestions(unansweredQuestions)
       setQuestionsLoading(false)
-
     }
     doGetUnansweredQuestions()
   }, [])
+
+  const handleAskQuestion = function() {
+    return console.log('Todo - move to ask page')
+  }
 
   return (
     <div>
       <div>
         <h2>Unanswered Questions</h2>
-        <button>Ask a question</button>
+        <button onClick={handleAskQuestion}>Ask a questio</button>
       </div>
       {
       questionsLoading ? (
