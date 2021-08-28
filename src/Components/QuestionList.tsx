0c48e79798edb49bf2f2ee03b3da22495322625a
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-28 15:09:16 -0500

    Styled questionslist

--- src/Components/QuestionList.tsx
+++ src/Components/QuestionList.tsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+import { css} from '@emotion/react'
+import { accent2, gray5 } from '../css/Styles'
 import React from 'react'
 import { QuestionData } from '../MockData/QuestionsData'
 import {Question} from '../Components/Question'
@@ -8,7 +11,16 @@ interface Props {
 
 export const QuestionList = function({data}: Props) {
   return (
-    <ul>
+    <ul css={css`
+    list-style: none;
+    margin: 10px 0 0 0;
+    padding: 0px 20px;
+    background-color: #fff;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border-top: 3px solid ${accent2};
+    box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+    `}>
       {data.map(function(question) {
         return (
           <>
