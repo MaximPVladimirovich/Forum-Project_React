3b6999b6faf718691835a9146c1de4f7526f004d
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-28 17:53:02 -0500

    Finished styling question component

--- src/Pages/QuestionPage.tsx
+++ src/Pages/QuestionPage.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { gray3, gray6 } from '../css/Styles';
+import { gray6 } from '../css/Styles';
 import React from 'react'
 import { Page} from '../Components/Page'
 import { useParams} from 'react-router-dom'
@@ -32,13 +32,23 @@ export const QuestionPage = function() {
         border-radius: 4px;
         border: 1px solid ${gray6};
         box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
- `}>
+      `}>
       <div css={css`
         font-size: 19px;
         font-weight: bold;
         margin: 10px 0px 5px;
-`}>{question === null ? '' : question.title}
+      `}>{question === null ? '' : question.title}
         </div>
+        {question !== null && (<React.Fragment><p
+        css={css`
+        margin-top: 0px;
+        background-color: white;
+        `}
+        >{question.content}</p>
+        <div>
+          {`Asked by ${question.userName} on ${question.created.toLocaleDateString()} ${question.created.toLocaleTimeString()}`}
+          </div>
+          </React.Fragment>)}
       </div> 
     </Page>
   )
