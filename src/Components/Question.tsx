e9bc689d5d7458c4ecd296be1c9d3a68dcfd86cf
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-28 15:14:34 -0500

    added styling to question

--- src/Components/Question.tsx
+++ src/Components/Question.tsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { gray2, gray3 } from '../css/Styles';
 import React from 'react'
 import {QuestionData} from '../MockData/QuestionsData'
 
@@ -7,11 +10,17 @@ interface Props {
 
 export const Question = function({data}: Props) {
   return (
-    <div>
-      <div>
+    <div css={css`
+    padding: 10px 10px`}>
+      <div css={css`
+      padding: 10px 10px;
+      font-size: 19px`}>
         {data.title}
       </div>
-      <div>
+      <div css={css`
+      font-size: 12px;
+      font-style: italic;
+      color: ${gray3};`}>
         {`Asked by ${data.userName} on 
         ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()}`}
       </div>
