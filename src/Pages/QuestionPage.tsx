253442fef196595d0e75476daf0c4dfbb6678348
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-28 17:45:15 -0500

    Rendered questionpage

--- src/Pages/QuestionPage.tsx
+++ src/Pages/QuestionPage.tsx
@@ -18,12 +18,28 @@ export const QuestionPage = function() {
       const foundQuestion = await getQuestion(questionId);
       setQuestion(foundQuestion);
     };
+
     if(questionId) {
       doGetQuestion(Number(questionId));
     }
   }, [questionId]);
 
   return (
-    <Page>Questions Page {questionId}</Page>
+    <Page>
+      <div css={css`
+        background-color: white;
+        padding: 15px 20px 20px 20px;
+        border-radius: 4px;
+        border: 1px solid ${gray6};
+        box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+ `}>
+      <div css={css`
+        font-size: 19px;
+        font-weight: bold;
+        margin: 10px 0px 5px;
+`}>{question === null ? '' : question.title}
+        </div>
+      </div> 
+    </Page>
   )
 }
