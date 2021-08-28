736e040b25323342da8142a80639d77992f797ef
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-28 16:16:52 -0500

    Added basic routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,33 @@ import React from 'react';
 import { Header } from './Components/Header'
 import { HomePage } from './Pages/HomePage'
 
+// Routing
+import { BrowserRouter, Switch,  Route} from 'react-router-dom'
+import { AskPage } from './Pages/AskPage'
+import { SignInPage } from './Pages/SignInPage'
+import { SearchPage } from './Pages/SearchPage'
+import { QuestionPage } from './Pages/QuestionPage'
+
+
 import {fontFamily, fontSize, gray2} from './css/Styles'
 
 function App() {
   return (
+  <BrowserRouter>
     <div css={css`
     font-family: ${fontFamily};
     font-size: ${fontSize};
     color: ${gray2};
-    `
-    }>
+    `}>
       <Header />
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/ask" component={AskPage} />
+        <Route path="/signin" component={SignInPage } />
+        <Route path="/search" component={SearchPage } />
+      </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
