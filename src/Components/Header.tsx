3d60540a0fbe04db6a5ef565f590b2cc7c8f9c74
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-28 13:55:03 -0500

    styled header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -23,9 +23,46 @@ export const Header = function() {
     border-bottom: 1px solid ${gray5};
     box-shadow: 0 3px 7px 0 rgba(110, 112, 114, 0.21);
     `}>
-      <a href='./'>Chattaway</a>
-      <input type='text' placeholder='Search...' onChange={handleSearchInputChange}/>
-      <a href='./signin'><span>Sign in</span></a>
+
+      <a href='./' css={css`
+      font-size: 24px;
+      font-weight:bold;
+      color: ${gray1};
+      text-decoration: none
+      `}
+      >Chattaway</a>
+
+      <input css={ css`
+      box-sizing: border-box;
+      font-family: ${fontFamily};
+      font-size: ${fontSize};
+      padding: 8px 10px;
+      border: 1px solid ${gray5};
+      color: ${gray2};
+      background-color: white;
+      width:200px
+      height: 30px;
+      :focus {
+        outline-color: ${gray5};
+      }
+      `}
+      type='text' placeholder='Search...' onChange={handleSearchInputChange}/>
+      <a href='./signin'
+      css={css`
+      font-family: ${fontFamily};
+      font-size: ${fontSize};
+      padding: 5px 10px;
+      background-color: transparent;
+      color: ${gray2};
+      text-decoration: none;
+      cursor: pointer;
+      :focus {
+        outline-color: ${gray5};
+      }
+      span {
+        margin-left: 7px;
+      }
+      `}><span>Sign in</span></a>
     </div>
   )
 }
