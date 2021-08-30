057bac1de366403848ed612b951b0412ca134ee8
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-30 09:11:34 -0500

    Ask form completed

--- src/Pages/AskPage.tsx
+++ src/Pages/AskPage.tsx
@@ -1,9 +1,49 @@
 import React from 'react'
-import { Page} from '../Components/Page'
+import { Page } from '../Components/Page'
+import { useForm } from 'react-hook-form'
+import { Fieldset } from '../css/Styles'
+import { FieldLabel } from '../css/Styles'
+import { FieldInput } from '../css/Styles'
+import { FieldContainer } from '../css/Styles'
+import { FieldTextArea } from '../css/Styles'
+import { FormButtonContainer } from '../css/Styles'
+import { PrimaryButton } from '../css/Styles'
+
+type FormData = {
+  title: string;
+  content: string;
+}
 
 
-export const AskPage = function() {
+export const AskPage = function () {
+  const { register } = useForm<FormData>();
   return (
-    <Page title='Ask a question'>{null}</Page>
+    <Page title='Ask a question'>
+      <form>
+        <Fieldset>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">
+              Title
+            </FieldLabel>
+            <FieldInput
+              id="title"
+              type="text"
+              {...register("title")}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <FieldLabel>
+              Content
+            </FieldLabel>
+            <FieldTextArea id='content' {...register('content')} />
+          </FieldContainer>
+          <FormButtonContainer>
+            <PrimaryButton type="submit">
+              Submit Question
+            </PrimaryButton>
+          </FormButtonContainer>
+        </Fieldset>
+      </form>
+    </Page>
   )
 }
