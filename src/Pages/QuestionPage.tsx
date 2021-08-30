c30f49079e35b5a5cff8b5bf32901a9efbe43844
maximpvladimirovich <maxim.bleeker@gmail.com>
2021-08-30 09:33:52 -0500

    added form to question

--- src/Pages/QuestionPage.tsx
+++ src/Pages/QuestionPage.tsx
@@ -2,15 +2,27 @@
 import { css } from '@emotion/react';
 import { gray6 } from '../css/Styles';
 import React from 'react'
-import { Page} from '../Components/Page'
-import { useParams} from 'react-router-dom'
+import { Page } from '../Components/Page'
+import { useParams } from 'react-router-dom'
 import { QuestionData, getQuestion } from '../MockData/QuestionsData'
 import { AnswerList } from '../Components/AnswerList'
+import { useForm } from 'react-hook-form'
+import { Fieldset } from '../css/Styles'
+import { FieldContainer } from '../css/Styles'
+import { FieldLabel } from '../css/Styles'
+import { FieldTextArea } from '../css/Styles'
+import { FormButtonContainer } from '../css/Styles'
+import { PrimaryButton } from '../css/Styles'
 
-export const QuestionPage = function() {
+type FormData = {
+  content: string;
+}
+
+export const QuestionPage = function () {
+  const { register } = useForm<FormData>()
   const [question, setQuestion] = React.useState<QuestionData | null>(null);
 
-  const {questionId} = useParams()
+  const { questionId } = useParams()
 
   React.useEffect(() => {
     const doGetQuestion = async (
@@ -20,7 +32,7 @@ export const QuestionPage = function() {
       setQuestion(foundQuestion);
     };
 
-    if(questionId) {
+    if (questionId) {
       doGetQuestion(Number(questionId));
     }
   }, [questionId]);
@@ -34,24 +46,40 @@ export const QuestionPage = function() {
         border: 1px solid ${gray6};
         box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
       `}>
-      <div css={css`
+        <div css={css`
         font-size: 19px;
         font-weight: bold;
         margin: 10px 0px 5px;
       `}>{question === null ? '' : question.title}
         </div>
         {question !== null && (<React.Fragment><p
-        css={css`
+          css={css`
         margin-top: 0px;
         background-color: white;
         `}
         >{question.content}</p>
-        <div>
-          {`Asked by ${question.userName} on ${question.created.toLocaleDateString()} ${question.created.toLocaleTimeString()}`}
+          <div>
+            {`Asked by ${question.userName} on ${question.created.toLocaleDateString()} ${question.created.toLocaleTimeString()}`}
           </div>
           <AnswerList data={question.answers} />
-          </React.Fragment>)}
-      </div> 
+          <form>
+            <Fieldset>
+              <FieldContainer>
+                <FieldLabel>
+                  Your answer
+                </FieldLabel>
+                <FieldTextArea>
+                </FieldTextArea>
+                <FormButtonContainer>
+                  <PrimaryButton>
+                    Submit Answer
+                  </PrimaryButton>
+                </FormButtonContainer>
+              </FieldContainer>
+            </Fieldset>
+          </form>
+        </React.Fragment>)}
+      </div>
     </Page>
   )
 }
